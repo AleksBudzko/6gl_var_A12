49eff2ffd8af83c2e1b37b4960e703b8f890f65c
Aleks Budzko <alexrfictcb@gmail.com>
2024-11-15 23:08:26 +0300

    каркас проекта

--- src/Project_6GL/Tech.java
+++ src/Project_6GL/Tech.java
@@ -1,5 +1,19 @@
 package Project_6GL;
 
+import java.util.Scanner;
+
+interface Technic{
+
+}
+
+abstract class Player implements Technic{
+
+}
+
+class VideoPlayer extends Player{
+
+}
+
 public class Tech {
 
 }
