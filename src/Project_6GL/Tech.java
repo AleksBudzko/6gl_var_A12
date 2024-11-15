99acd785ab4f12121326b8ba6ba847301f45faf1
Aleks Budzko <alexrfictcb@gmail.com>
2024-11-15 23:30:34 +0300

    каркас проекта N1

--- src/Project_6GL/Tech.java
+++ src/Project_6GL/Tech.java
@@ -2,18 +2,33 @@ package Project_6GL;
 
 import java.util.Scanner;
 
+//Интерфейс общий для всей техники
 interface Technic{
+boolean IsTurnedOn();
 
 }
-
+//Абстрактный класс для всех возможных плееров
 abstract class Player implements Technic{
 
 }
 
+//Класс видеоплеера
 class VideoPlayer extends Player{
+    boolean TurnedOn;
+    private final String name;
+    public VideoPlayer(String name){
+        this.name=name;
+    }
+
+@Override
+public boolean IsTurnedOn(){return TurnedOn;}
 
 }
 
+//Основной класс
 public class Tech {
+    public static void main(String[] args) {
 
+
+    }
 }
